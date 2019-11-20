d45d765b2384c4c1e59417ca6c0ca9a434fd83d5
PCMBarber <Kochab.96@Gmail.com>
2019-11-20 16:19:00 +0000

    added demonstration of inheritence

--- Playing with classes.py
+++ Playing with classes.py
@@ -1,57 +1,109 @@
 import time
 import os
+from abc import ABC, abstractmethod
 
-class person():
+class person():                                                             #Super class 
     def __init__(self, name, last_name):
         self.name = name
         self.last_name = last_name
-
-class student(person):
-    def __init__(self, name1, last_name, maths1, science1, english1):
-        super().__init__(name1, last_name)
-        self.english = english1
-        self.maths = maths1
-        self.science = science1
-
+        
+    @abstractmethod
     def get_average(self):
+        pass
+
+class student(person):                                                      #object constructor for students
+    def __init__(self, name, last_name, maths, science, english):
+        super().__init__(name, last_name)   #declares name variables from person()
+        self.english = english
+        self.maths = maths
+        self.science = science
+
+    def get_average(self):                 #overriding function
         average = self.english + self.maths + self.science
         average = int(average / 3)
         return average
+    
+class teacher(person):                                                      #object constructor for teachers
+    def __init__(self, name, last_name, av_english, av_maths, av_science):
+        super().__init__(name, last_name)  #declares name variables from person()
+        self.av_english = av_english
+        self.av_maths = av_maths
+        self.av_science = av_science
 
+    def get_average(self):                #overriding function
+        average = self.av_english + self.av_maths + self.av_science
+        average = int(average / 3)
+        return average
+    
 def main():
-    name = input("Please enter student's first name: ")
-    last_name = input("Please enter student's last name: ")
-    validator = False
-    
-    while validator == False:
-        english = input("Please enter english grade: ")
-        validator = int_validator(english)
-    english = int(english)
-    validator = False
-    
-    while validator == False:
-        maths = input("Please enter maths grade: ")
-        validator = int_validator(maths)
-    maths = int(maths)
-    validator = False
-    
-    while validator == False:
-        science = input("Please enter science grade: ")
-        validator = int_validator(science)
-    science = int(science)
-    
-    new_student = student(name, last_name, maths, science, english)
+    selection = input("please select (T)eacher or (S)tudent: ").lower()  #Select path with elif
 
-    average_grade = new_student.get_average()
+    if selection == "s":
+        name = input("Please enter student's first name: ")
+        last_name = input("Please enter student's last name: ")
+        validator = False
+            
+        while validator == False:                                      #while loop only broken by int_validator()
+            english = input("Please enter english grade: ")
+            validator = int_validator(english)
+        english = int(english)
+        validator = False
+            
+        while validator == False:
+            maths = input("Please enter maths grade: ")
+            validator = int_validator(maths)
+        maths = int(maths)
+        validator = False
+            
+        while validator == False:
+            science = input("Please enter science grade: ")
+            validator = int_validator(science)
+        science = int(science)
+            
+        new_student = student(name, last_name, maths, science, english)   #Create new instance
 
-    print(name, "has an average grade of: ", average_grade)
+        average_grade = new_student.get_average()                         #calculates average with overidden function
+
+        print(new_student.name, new_student.last_name, "has an average grade of: ", average_grade)
+            
+    elif selection == "t":       #Exact same as student but taking different values
+        name = input("Please enter teacher's first name: ")
+        last_name = input("Please enter teacher's last name: ")
+        validator = False
+
+        while validator == False:
+            av_english = input("Please enter average class english grade: ")
+            validator = int_validator(av_english)
+        av_english = int(av_english)
+        validator = False
+
+        while validator == False:
+            av_maths = input("Please enter average class maths grade: ")
+            validator = int_validator(av_maths)
+        av_maths = int(av_maths)
+        validator = False
+
+        while validator == False:
+            av_science = input("Please enter average class science grade: ")
+            validator = int_validator(av_science)
+        av_science = int(av_science)
+
+        new_teacher = teacher(name, last_name, av_english, av_maths, av_science)
+
+        average_grade = new_teacher.get_average()
+
+        print(new_teacher.name, new_teacher.last_name, "has an average class grade of: ", average_grade)
+
+    else:
+        print("Invalid input")
+        main()
 
     time.sleep(5)
     clear()
-    
+        
     use_again()
 
-def int_validator(num):
+def int_validator(num):    #Auto validates int user inputs
     try:
         num = int(num)
         return True
@@ -59,7 +111,7 @@ def int_validator(num):
         print("Invalid input")
         return False
 
-def use_again():
+def use_again():       #Simple elif to rerun code as many times as desired
     while True:
         again = input("Do you want to enter another? (Y/N) : ").lower()
         if again == "y":
@@ -72,7 +124,7 @@ def use_again():
             print("Invalid Input")
             continue
 
-def clear():
+def clear():      #Clears screen on linux or windows
     if os.name == 'nt':
         os.system('CLS')
     if os.name == 'posix':
